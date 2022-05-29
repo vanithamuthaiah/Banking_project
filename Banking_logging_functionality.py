5ac176a49029b04daeff5754a2892e6f45e266cd
vanithamuthaiah <92114359+vanithamuthaiah@users.noreply.github.com>
2022-05-29 16:44:55 +0200

    Added Admin_delete_method

--- Banking_logging_functionality.py
+++ Banking_logging_functionality.py
@@ -1,9 +1,10 @@
 import sqlite3 as sl
 import sys
-
+#connecting with database
 con=sl.connect('usersarchive3.db')
 cur = con.cursor()
 '''
+#within quotes because the table dont want to be created whenever the code runs
 #with cur:
 cur.execute("""
     CREATE TABLE USER (
@@ -17,7 +18,7 @@ cur.execute("""
     );
 """)
 
-
+#inserting data into database
 sql = 'INSERT INTO USER (Account_num, Username, Balance, Address,Phone_num,Password) values(?,?,?,?,?,?)'
 data=[(100,'Vani',1000,'xyz',123456,'123'),
           (101,'Mani',2000,'xyk',123456,'1234')]
@@ -36,7 +37,7 @@ with con:
         );
     """)
 
-
+#creating table for Admin
 sql1 = 'INSERT INTO Admins7_Info (ID, Username,Password) values(?,?,?)'
 data1=[(22001,'Janu','123'),
           (22002,'Manu','1234'),
@@ -49,12 +50,12 @@ class Logging:
 
     def __init__(self):
         self.user = self.get_user_type()
-
+    #ask Customer or Admin
     def get_user_type(self):
         print('Welcome to XYZ Bank\n 1.Customer 2.Admin 3.Exit')
         user = input('\n Who are you? Please enter the number: ')
         return user
-
+    #welcome page and call customer area method
     def welcome_page(self):
         if self.user == '1':
             print('\n ***welcome to customer area*** \n')
@@ -65,14 +66,14 @@ class Logging:
             return data1.admin_area()
         elif self.user == '3':
             SystemExit
-
+    #ask customer for process
     def customer_area(self):
         entry = input('Please choose from the option \n 1.Login \n 2.Sign up \n 3.Previous Menu \n 4.Exit \n')
-        data = Customer()
+        data = Customer()#cretaed object for customer class
         if entry == '1':
-            return data.login()
+            return data.login()#call login method from customer class
         elif entry=='2':
-            data.sign_up()
+            data.sign_up()#call sign_up method from customer class
         elif entry=='3':
             return self.get_user_type()
         else:
@@ -100,7 +101,7 @@ class Customer:
                         password=input('Please enter the password: ')
                         db_password=cur.execute(f"SELECT Password FROM USER WHERE Username = '{self.user_name}'")
                         for db_pw in db_password:
-                            if db_pw[0] == password:
+                            if db_pw[0] == password:#selecting [0] as it saved as tuples
                                 print('welcome!!!')
                                 return self.mainmenu()
                             else:
@@ -152,7 +153,16 @@ class Customer:
         Username = input('enter your Username: ')
         Balance = int(input('enter your balance: '))
         Address=input('Enter your address: ')
-        Phone_num=int(input('Enter your Phone Number: '))
+        Phone_num=''
+        counter=0
+        while counter<3:
+            Phone_num = input('Enter your Phone Number: ')
+            if Phone_num.isnumeric():
+                break
+            else:
+                print('Please enter a valid number!!')
+            counter=+1
+
         Password=input('Enter your Password: ')
         cur.execute("""
         INSERT INTO USER(Username, Balance, Address, Phone_num, Password )
@@ -170,7 +180,7 @@ class Admin:
         def admin_area(self):
             entry= input('Please choose from the option \n 1.Login \n 2.Previous Menu \n 3.Exit \n')
             if entry == '1':
-                self.admin_username=input('Please enter your Username: \n')
+                self.admin_username=input('Please enter your Admin Credential: \n')
                 with con:
                     db_adminlist = cur.execute("SELECT Username FROM Admins7_Info")
                     user_found=False
@@ -194,8 +204,6 @@ class Admin:
             if user_found==False:
                 print('Username is unavailable')
 
-                #self.admin_main_menu()
-
             elif entry== '2':
                 return self.user_type()
             else:
@@ -203,40 +211,40 @@ class Admin:
 
         def admin_main_menu(self):
             print(f'*****Welcome  { self.admin_username} *****')
-            process = input('Please choose the number \n 1.Customer Details \n 2.Delete The Customer Details \n 3.Add The New Customer Details \n')
+            process = input('Please choose the number \n 1.Customer Details  \n 2.Add The New Customer Details \n 3.Delete the Customer Details \n')
             
             if process =='1':
-                #my_conn = my_connect.cursor()
                 users=cur.execute("SELECT Username, Address, Phone_num FROM USER ")
-                #columns = cur.fetchall()
-                #print(users)
-
-                i=0
                 for user in users:
-                    for j in range(len(user)):
-                        print(user[j],end=' ')
-                    i=i+1
+                    print('Username:' + user[0])
+                    print('Address:' + user[1])
+                    print('Phone_num:' , user[2])
                     print()
             
-            elif process == '3' :
-                Username = input('enter your Username: ')
-                Balance = int(input('enter your balance: '))
-                Address=input('Enter your address: ')
-                Phone_num=int(input('Enter your Phone Number: '))
-                Password=input('Enter your Password: ')
+            elif process == '2' :
+                Username = input('enter the Username: ')
+                Balance = int(input('enter the balance: '))
+                Address=input('Enter the address: ')
+                Phone_num=''
+                counter=0
+                while counter<3:
+                    Phone_num = input('Enter your Phone Number: ')
+                    if Phone_num.isnumeric():
+                        break
+                    else:
+                        print('Please enter a valid number!!')
+                    counter=+1
+                Password=input('Enter the Password: ')
                 cur.execute("""
                 INSERT INTO USER(Username, Balance, Address, Phone_num, Password )
                 VALUES (?,?,?,?,?)
                 """, (Username, Balance, Address, Phone_num, Password ))
                 con.commit ()
-                print ( '****Data Signed up successfully!!!**** ' )                
-  
+                print ( '****Data Signed up successfully!!!**** ' )  
+                
+            elif process == '3':
+                username_delete=(input('Enter the username to delete: '))
+                cur.execute(f"DELETE from USER WHERE Username = '{username_delete}' " )
+                
 d = Logging()
 d.welcome_page()
-
-''' elif process == '2':
-                username_delete=(input('Enter the username to delete: '))
-                columns = cur.fetchall()
-                delete_box=cur.execute(f"DELETE from USER WHERE Username = {username_delete}" )
-                delete_box.delete(0, END)
-'''
